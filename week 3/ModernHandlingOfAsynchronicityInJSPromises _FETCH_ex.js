953f6eaa38f6b381d238882a1c2e899982055e65
lukaszzie <l.ziemann@cyberfolks.pl>
2022-10-25 16:08:09 +0200

    add ModernHandlingOfAsynchronicityInJSPromises _FETCH_ex.js for week3

---
+++ week 3/ModernHandlingOfAsynchronicityInJSPromises _FETCH_ex.js
@@ -0,0 +1,15 @@
+/**
+Przyjmij w przeglądarce przez prompt-a ISBN. Następnie korzystając z poniższego API:
+https://www.googleapis.com/books/v1/volumes?q=isbn i na końcu numer ISBN.
+Wyświetl zwracane dane. Wyświetl w konsoli autora i tytuł książki.
+**/
+
+const url = 'https://www.googleapis.com/books/v1/volumes?q=3-932949-11-0'
+
+const request = fetch(url);
+const jsonFormat = response => response.json();
+const whichData = data => console.log(`Tytuł: ${data.items[0].volumeInfo.title} \n
+Autor: ${data.items[0].volumeInfo.authors}`);
+const error = error => console.log(error);
+
+request.then(jsonFormat).then(whichData).catch(error);
